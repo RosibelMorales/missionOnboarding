a6e9be5f18a02a709f32d35066b30846663c994c
Rosibel Morales <rosibelm8217@gmail.com>
2022-02-22 19:46:55 -0600

    Se agrega el proyecto

---
+++ Mod 1/program1.py
@@ -0,0 +1,15 @@
+print('Hola desde consola, esto es Python en Visual Studio Code :)')
+
+sum = 1 + 2
+print(sum)
+
+producto = sum * 2
+print(producto)
+
+from datetime import date
+print('La fecha de hoy es: ' + str(date.today()))
+
+print("Bienvenido al programa de bienvenida!")
+name = input("Ingrese su nombre: ")
+print("Saludos " + name)
+
